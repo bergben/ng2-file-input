205a974996f247f10c075f0650b4a7a0711267d0
Benedikt Berger <benedikt.berger@live.de>
2017-11-16 22:32:22 +0100

    bugfix ng5

--- ng2-file-input.ts
+++ ng2-file-input.ts
@@ -1,6 +1,7 @@
 export { Ng2FileInputComponent } from './src/ng2-file-input.component';
 export { FileInputHandlerService } from './src/file-input-handler.service';
 export { Ng2FileInputService } from './src/ng2-file-input.service';
+export { Ng2FileInputOptionsInterface } from './src/ng2-file-input-options.interface';
 export { Ng2FileInputOptions } from './src/ng2-file-input-options.class';
 export { Ng2FileInputAction } from './src/ng2-file-input-action.enum';
 export { Ng2FileInputModule } from './src/ng2-file-input.module';
